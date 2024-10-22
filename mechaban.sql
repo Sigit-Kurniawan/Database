2edbdc2a86dd756545bbfc4ef7666fefddf7b40c
CitraRaahayu <158290632+CitraRaahayu@users.noreply.github.com>
2024-10-23 00:25:05 +0700

    nambahin view

--- mechaban.sql
+++ mechaban.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 22, 2024 at 05:09 PM
+-- Generation Time: Oct 22, 2024 at 05:24 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -42,8 +42,8 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id_account`, `name`, `email`, `no_hp`, `password`, `role`, `photo`) VALUES
-('EQHSO', 'Dara', 'daraa@gmail.com', '098765', 'daradara', 'customer', NULL),
-('KAEUZ', 'Citra12', 'citra@gmail.com', '12345678', '123', 'customer', NULL),
+('ACBY', 'Rahayu', 'rahayu@gmail.com', '034567899', 'rahayuu', 'montir', NULL),
+('KAEUZ', 'Citra', 'citra@gmail.com', '12345678', '123', 'customer', NULL),
 ('QOSGN', 'Dara', 'dara@gmail.com', '098765', 'daradara', 'customer', NULL);
 
 -- --------------------------------------------------------
@@ -72,6 +72,13 @@ CREATE TABLE `booking` (
   `nopol` varchar(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`id_booking`, `tgl_booking`, `total_biaya`, `metode_bayar`, `nopol`) VALUES
+('b1102', '2024-10-17', 45000, 'Tunai', 'A 1234 BC');
+
 -- --------------------------------------------------------
 
 --
@@ -87,6 +94,13 @@ CREATE TABLE `car` (
   `id_account` char(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `car`
+--
+
+INSERT INTO `car` (`nopol`, `merk`, `type`, `transmition`, `year`, `id_account`) VALUES
+('A 1234 BC', 'Honda', '-', 'manual', 2018, 'KAEUZ');
+
 -- --------------------------------------------------------
 
 --
@@ -98,9 +112,17 @@ CREATE TABLE `detail_booking` (
   `id_booking` char(5) NOT NULL,
   `id_montir` char(5) NOT NULL,
   `id_layanan_servis` char(5) NOT NULL,
-  `id_barang` char(5) NOT NULL,
+  `id_barang` char(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `subtotal` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `detail_booking`
+--
+
+INSERT INTO `detail_booking` (`id_detail_booking`, `id_booking`, `id_montir`, `id_layanan_servis`, `id_barang`, `subtotal`) VALUES
+('BK001', 'b1102', 'MON01', 'LV001', NULL, 20000),
+('BK002', 'b1102', 'MON01', 'LV002', NULL, 25000);
 
 -- --------------------------------------------------------
 
@@ -112,6 +134,14 @@ CREATE TABLE `jenis_servis` (
   `id_jenis_servis` char(5) NOT NULL,
   `nama_servis` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `jenis_servis`
+--
+
+INSERT INTO `jenis_servis` (`id_jenis_servis`, `nama_servis`) VALUES
+('SV111', 'Ban'),
+('SV112', 'Oli');
 
 -- --------------------------------------------------------
 
@@ -125,6 +155,15 @@ CREATE TABLE `layanan_servis` (
   `nama_layanan` varchar(30) NOT NULL,
   `harga_layanan` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `layanan_servis`
+--
+
+INSERT INTO `layanan_servis` (`id_jenis_servis`, `id_layanan_servis`, `nama_layanan`, `harga_layanan`) VALUES
+('SV111', 'LV001', 'Tambal Ban', 20000),
+('SV111', 'LV002', 'Gant Ban', 25000),
+('SV112', 'LV003', 'Ganti Oli', 30000);
 
 -- --------------------------------------------------------
 
@@ -140,6 +179,81 @@ CREATE TABLE `montir` (
   `no_hp` varchar(13) NOT NULL,
   `password` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `montir`
+--
+
+INSERT INTO `montir` (`id_montir`, `id_account`, `nama_montir`, `email`, `no_hp`, `password`) VALUES
+('MON01', 'ACBY', 'Rahayu', 'rahayu@gmail.com', '09876543', 'rahayuu');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_montir`
+-- (See below for the actual view)
+--
+CREATE TABLE `view_montir` (
+`id_montir` char(5)
+,`nama_montir` tinytext
+,`tgl_booking` date
+,`total_order` bigint
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_penghasilan_perhari`
+-- (See below for the actual view)
+--
+CREATE TABLE `view_penghasilan_perhari` (
+`tgl_booking` date
+,`total_penghasilan` decimal(32,0)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_riwayat_customer`
+-- (See below for the actual view)
+--
+CREATE TABLE `view_riwayat_customer` (
+`id_customer` char(5)
+,`merk` varchar(20)
+,`nama_customer` tinytext
+,`nama_layanan` varchar(30)
+,`nopol` varchar(9)
+,`subtotal` int
+,`tgl_booking` date
+,`type` varchar(20)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_montir`
+--
+DROP TABLE IF EXISTS `view_montir`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_montir`  AS SELECT `m`.`id_montir` AS `id_montir`, `m`.`nama_montir` AS `nama_montir`, `b`.`tgl_booking` AS `tgl_booking`, count(`b`.`id_booking`) AS `total_order` FROM ((`montir` `m` join `detail_booking` `db` on((`m`.`id_montir` = `db`.`id_montir`))) join `booking` `b` on((`db`.`id_booking` = `b`.`id_booking`))) GROUP BY `m`.`id_montir`, `m`.`nama_montir`, `b`.`tgl_booking``tgl_booking`  ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_penghasilan_perhari`
+--
+DROP TABLE IF EXISTS `view_penghasilan_perhari`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_penghasilan_perhari`  AS SELECT `b`.`tgl_booking` AS `tgl_booking`, sum(`db`.`subtotal`) AS `total_penghasilan` FROM (`booking` `b` join `detail_booking` `db` on((`b`.`id_booking` = `db`.`id_booking`))) GROUP BY `b`.`tgl_booking``tgl_booking`  ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_riwayat_customer`
+--
+DROP TABLE IF EXISTS `view_riwayat_customer`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_riwayat_customer`  AS SELECT `a`.`id_account` AS `id_customer`, `a`.`name` AS `nama_customer`, `b`.`tgl_booking` AS `tgl_booking`, `b`.`nopol` AS `nopol`, `c`.`merk` AS `merk`, `c`.`type` AS `type`, `ls`.`nama_layanan` AS `nama_layanan`, `db`.`subtotal` AS `subtotal` FROM ((((`account` `a` join `car` `c` on((`a`.`id_account` = `c`.`id_account`))) join `booking` `b` on((`c`.`nopol` = `b`.`nopol`))) join `detail_booking` `db` on((`b`.`id_booking` = `db`.`id_booking`))) join `layanan_servis` `ls` on((`db`.`id_layanan_servis` = `ls`.`id_layanan_servis`))) WHERE (`a`.`role` = 'customer')  ;
 
 --
 -- Indexes for dumped tables
